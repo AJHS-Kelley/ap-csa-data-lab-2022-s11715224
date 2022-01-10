92adb6b5b443dfb5fc758bbc3dc61182330ecae6
Rachel. M <70598758+s11715224@users.noreply.github.com>
2022-01-10 09:31:50 -0500

    v.0.2
    uh-

--- Cereal.java
+++ Cereal.java
@@ -1,4 +1,4 @@
-// robin/rachel macdonald, AP Data Lab 1/10/2022 v.0.1
+// robin/rachel macdonald, AP Data Lab 1/10/2022 v.0.2
 
 public class Cereal {
   
@@ -7,4 +7,22 @@ public class Cereal {
     private double cup;
     private double fiberPerCup;
 
+    public Cereal(String cerealName, double fiber, double cup, double fiberPerCup){
+        
+        name= cerealName;
+        fiber = fiber;
+        cup = cup;
+        FPC = fiberPerCup;
+
+    }
+
+    public Cereal(){
+
+        name = "";
+        fiber = 0.0;
+        cup = 0.0;
+        FPC = 0.0;
+
+
+    }
 }
