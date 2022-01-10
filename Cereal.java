cefa39a623fb11603d31a75114900a5757c3dd7b
Rachel. M <70598758+s11715224@users.noreply.github.com>
2022-01-10 09:18:30 -0500

    v.0.1
    added string, double, double, and double

--- Cereal.java
+++ Cereal.java
@@ -1,5 +1,10 @@
 // robin/rachel macdonald, AP Data Lab 1/10/2022 v.0.1
 
 public class Cereal {
-    
+  
+    private String cerealName;
+    private double fiber;
+    private double cup;
+    private double fiberPerCup;
+
 }
